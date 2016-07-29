79e2a745c703d37e7b0b960f3153269b6694c28a
yukisov <yukisov@gmail.com>
2016-07-29 23:55:10 +0900

    Add .bookignore and some utils

---
+++ utils/make_img_links.js
@@ -0,0 +1,32 @@
+var fs = require('fs'),
+    path = require('path'),
+    glob = require("glob")
+var options = {}
+var content = ""
+
+var join = path.posix.join,
+    relative = path.posix.relative
+
+
+if (process.argv.length <= 2) {
+  console.log('You need one parameter.');
+  process.exit();
+}
+//console.log(process.argv[2]);
+
+var dirPath = join("./", process.argv[2], "/**/*.png");
+
+//console.log(dirPath);
+
+
+// options is optional
+var filenames = glob.sync(dirPath, options)
+//console.log(filenames);
+filenames.forEach(function(filename) {
+
+  //console.log(filename);
+  content += "![image](" + relative('.', filename) + ")\n"
+});
+
+fs.writeFileSync("./make_img_links.txt", content);
+
